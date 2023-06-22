678c15b3bff303dbea1026819583c9c8e4469b05
neonxflame <124432647+shermanTWQ@users.noreply.github.com>
2023-06-23 01:22:51 +0800

    Add files via upload

---
+++ BeAwake/EcoGreen/Form1.Designer.vb
@@ -0,0 +1,90 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.btnAwake = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblStatus = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnAwake
+        '
+        Me.btnAwake.Location = New System.Drawing.Point(47, 113)
+        Me.btnAwake.Name = "btnAwake"
+        Me.btnAwake.Size = New System.Drawing.Size(125, 50)
+        Me.btnAwake.TabIndex = 0
+        Me.btnAwake.Text = "Disable BeAware"
+        Me.btnAwake.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(44, 93)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(52, 17)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Status:"
+        '
+        'lblStatus
+        '
+        Me.lblStatus.AutoSize = True
+        Me.lblStatus.Location = New System.Drawing.Point(102, 93)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(72, 17)
+        Me.lblStatus.TabIndex = 5
+        Me.lblStatus.Text = "Not Active"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(77, 23)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(51, 50)
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(221, 175)
+        Me.Controls.Add(Me.lblStatus)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.btnAwake)
+        Me.Name = "Form1"
+        Me.Text = "BeAwake 1.0"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnAwake As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblStatus As Label
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ BeAwake/EcoGreen/Form1.vb
@@ -0,0 +1,30 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class Form1
+
+    'BeAware 1.0
+    'Provided for non-commercial use. Please refer to Github Repo for more information. 
+    'Contact: Sherman_tan@outlook.com
+    '(C) Tan Wei Qiang Sherman.
+
+    <FlagsAttribute()>
+    Public Enum EXECUTION_STATE As UInteger
+        ES_SYSTEM_REQUIRED = &H1
+        ES_DISPLAY_REQUIRED = &H2
+        ES_CONTINUOUS = &H80000000UI
+    End Enum
+
+    <DllImport("Kernel32.DLL", CharSet:=CharSet.Auto, SetLastError:=True)>
+    Private Shared Function SetThreadExecutionState(ByVal state As EXECUTION_STATE) As EXECUTION_STATE
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        SetThreadExecutionState(EXECUTION_STATE.ES_SYSTEM_REQUIRED)
+        lblStatus.Text = "Active"
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnAwake.Click
+        lblStatus.Text = "Not Active"
+        Me.Close()
+    End Sub
+End Class
